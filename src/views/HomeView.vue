073830d81f5edd255c63a07002c6e373d8763140
Gloumy <simon@cornet.dev>
2023-09-29 22:05:36 +0200

    play sound before step change

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,10 +2,13 @@
 import { Card } from '@/models/Card/Card'
 import { ref } from 'vue';
 
+import { useSound } from '@vueuse/sound'
+import countdownSound from '@/assets/sounds/countdown-4.mp3'
+
 const mockCard = new Card().mock();
 const currentStep = ref(-1);
 const currentStepCountdown = ref(0);
-
+const { play } = useSound(countdownSound)
 async function timer() {
   const maxIndex = mockCard.steps.length - 1;
   for (let i = 0; i <= maxIndex; i++) {
@@ -13,6 +16,7 @@ async function timer() {
     const duration = mockCard.steps[i].duration;
     for (let j = duration; j >= 0; j--) {
       currentStepCountdown.value = j;
+      if (j == 3) play();
       await new Promise(r => setTimeout(r, 1000))
     }
   }
