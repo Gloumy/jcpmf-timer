daa4ac774bf48a1eb2b094798747cfc67b8dbfd1
Gloumy <simon@cornet.dev>
2023-09-30 17:43:56 +0200

    define mock data as json

---
+++ src/data/cards05.ts
@@ -0,0 +1,306 @@
+export const cards05 = [
+    {
+        "weekNumber": 1,
+        "days": [
+            {
+                "day": 1,
+                "steps": [
+                    {
+                        "type": "E/S",
+                        "duration": 60000
+                    },
+                    {
+                        "type": "C",
+                        "duration": 6000
+                    },
+                    {
+                        "type": "M",
+                        "duration": 6000
+                    },
+                    {
+                        "type": "C",
+                        "duration": 24000
+                    },
+                    {
+                        "type": "M",
+                        "duration": 12000
+                    },
+                    {
+                        "type": "C",
+                        "duration": 30000
+                    },
+                    {
+                        "type": "M",
+                        "duration": 18000
+                    },
+                    {
+                        "type": "C",
+                        "duration": 36000
+                    },
+                    {
+                        "type": "M",
+                        "duration": 18000
+                    },
+                    {
+                        "type": "C",
+                        "duration": 24000
+                    },
+                    {
+                        "type": "RC",
+                        "duration": 30000
+                    }
+                ]
+            },
+            {
+                "day": 2,
+                "steps": [
+                    {
+                        "type": "E/S",
+                        "duration": 60000
+                    },
+                    {
+                        "type": "C",
+                        "duration": 6000
+                    },
+                    {
+                        "type": "M",
+                        "duration": 6000
+                    },
+                    {
+                        "type": "C",
+                        "duration": 24000
+                    },
+                    {
+                        "type": "M",
+                        "duration": 12000
+                    },
+                    {
+                        "type": "C",
+                        "duration": 30000
+                    },
+                    {
+                        "type": "M",
+                        "duration": 18000
+                    },
+                    {
+                        "type": "C",
+                        "duration": 36000
+                    },
+                    {
+                        "type": "M",
+                        "duration": 18000
+                    },
+                    {
+                        "type": "C",
+                        "duration": 24000
+                    },
+                    {
+                        "type": "RC",
+                        "duration": 30000
+                    }
+                ]
+            },
+            {
+                "day": 3,
+                "steps": [
+                    {
+                        "type": "E/S",
+                        "duration": 60000
+                    },
+                    {
+                        "type": "C",
+                        "duration": 6000
+                    },
+                    {
+                        "type": "M",
+                        "duration": 6000
+                    },
+                    {
+                        "type": "C",
+                        "duration": 24000
+                    },
+                    {
+                        "type": "M",
+                        "duration": 12000
+                    },
+                    {
+                        "type": "C",
+                        "duration": 30000
+                    },
+                    {
+                        "type": "M",
+                        "duration": 18000
+                    },
+                    {
+                        "type": "C",
+                        "duration": 36000
+                    },
+                    {
+                        "type": "M",
+                        "duration": 18000
+                    },
+                    {
+                        "type": "C",
+                        "duration": 24000
+                    },
+                    {
+                        "type": "RC",
+                        "duration": 30000
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        "weekNumber": 2,
+        "days": [
+            {
+                "day": 1,
+                "steps": [
+                    {
+                        "type": "E/S",
+                        "duration": 60000
+                    },
+                    {
+                        "type": "C",
+                        "duration": 6000
+                    },
+                    {
+                        "type": "M",
+                        "duration": 6000
+                    },
+                    {
+                        "type": "C",
+                        "duration": 24000
+                    },
+                    {
+                        "type": "M",
+                        "duration": 12000
+                    },
+                    {
+                        "type": "C",
+                        "duration": 30000
+                    },
+                    {
+                        "type": "M",
+                        "duration": 18000
+                    },
+                    {
+                        "type": "C",
+                        "duration": 36000
+                    },
+                    {
+                        "type": "M",
+                        "duration": 18000
+                    },
+                    {
+                        "type": "C",
+                        "duration": 24000
+                    },
+                    {
+                        "type": "RC",
+                        "duration": 30000
+                    }
+                ]
+            },
+            {
+                "day": 2,
+                "steps": [
+                    {
+                        "type": "E/S",
+                        "duration": 60000
+                    },
+                    {
+                        "type": "C",
+                        "duration": 6000
+                    },
+                    {
+                        "type": "M",
+                        "duration": 6000
+                    },
+                    {
+                        "type": "C",
+                        "duration": 24000
+                    },
+                    {
+                        "type": "M",
+                        "duration": 12000
+                    },
+                    {
+                        "type": "C",
+                        "duration": 30000
+                    },
+                    {
+                        "type": "M",
+                        "duration": 18000
+                    },
+                    {
+                        "type": "C",
+                        "duration": 36000
+                    },
+                    {
+                        "type": "M",
+                        "duration": 18000
+                    },
+                    {
+                        "type": "C",
+                        "duration": 24000
+                    },
+                    {
+                        "type": "RC",
+                        "duration": 30000
+                    }
+                ]
+            },
+            {
+                "day": 3,
+                "steps": [
+                    {
+                        "type": "E/S",
+                        "duration": 60000
+                    },
+                    {
+                        "type": "C",
+                        "duration": 6000
+                    },
+                    {
+                        "type": "M",
+                        "duration": 6000
+                    },
+                    {
+                        "type": "C",
+                        "duration": 24000
+                    },
+                    {
+                        "type": "M",
+                        "duration": 12000
+                    },
+                    {
+                        "type": "C",
+                        "duration": 30000
+                    },
+                    {
+                        "type": "M",
+                        "duration": 18000
+                    },
+                    {
+                        "type": "C",
+                        "duration": 36000
+                    },
+                    {
+                        "type": "M",
+                        "duration": 18000
+                    },
+                    {
+                        "type": "C",
+                        "duration": 24000
+                    },
+                    {
+                        "type": "RC",
+                        "duration": 30000
+                    }
+                ]
+            }
+        ]
+    }
+]
