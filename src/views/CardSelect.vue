97c11f0c90a11e6fd3c3d261b818159bc82def93
Gloumy <simon@cornet.dev>
2023-09-30 17:48:52 +0200

    wip- card selection

---
+++ src/views/CardSelect.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { JsonConvert } from 'json2typescript'
+import { cards05 } from '@/data/cards05'
+import { Week } from '@/models/Card/Week';
+
+let jsonConvert: JsonConvert = new JsonConvert();
+const weeks = jsonConvert.deserializeArray(cards05, Week);
+
+</script>
+
+<template>
+    <v-expansion-panels>
+        <v-expansion-panel v-for="week in weeks" :key="week.weekNumber" :title="week.displayTitle()">
+            <template v-slot:text>
+                <span class="d-flex justify-space-around"><v-btn variant="text" v-for="day in week.days" :key="day.day">{{ day.day
+                }}</v-btn></span>
+            </template>
+
+        </v-expansion-panel>
+    </v-expansion-panels>
+</template>
